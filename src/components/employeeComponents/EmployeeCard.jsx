138963a5850929a99a97207b577f582647ef5cdf
David Rotariu <davidrotariu139@gmail.com>
2024-03-02 14:16:27 +0200

    sign up employee

--- src/components/employeeComponents/EmployeeCard.jsx
+++ src/components/employeeComponents/EmployeeCard.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
-import { Card, Avatar, Modal, Badge, Button } from '@mantine/core';
+import { Card, Avatar, Modal, Badge, Button, Text, Title } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { useState } from 'react'
 
 export default function EmployeeCard({ employee }) {
 
     const [opened, { open, close }] = useDisclosure(false);
+    const [isHovering, setIsHovering] = useState(false);
     const getInitials = (name) => {
         const names = name.split(' ');
         return names.map((name) => name[0]).join('').toUpperCase();
@@ -25,18 +28,21 @@ export default function EmployeeCard({ employee }) {
                 </div>
 
                 <div className="pt-4 flex justify-center">
-                    { !employee.user_roles.length && (<Badge color="gray" size="xl" variant="filled">Employee</Badge>) }
-                    { employee.user_roles.includes("admin") && (<Badge color="gray" size="xl" variant="filled">Organization Admin</Badge>) }
-                    { employee.user_roles.includes("dept_manager") && (<Badge color="gray" size="xl" variant="filled">Department Manager</Badge>) }
-                    { employee.user_roles.includes("proj_manager") && (<Badge color="gray" size="xl" variant="filled">Project Manager</Badge>) }
+                    {!employee.user_roles.length && (<Badge color="gray" size="xl" variant="filled">Employee</Badge>)}
+                    {employee.user_roles.includes("admin") && (<Badge className="bg-[gray] text-white font-bold rounded-xl text-lg p-5">Organization Admin</Badge>)}
+                    {employee.user_roles.includes("dept_manager") && (<Badge color="gray" size="xl" variant="filled">Department Manager</Badge>)}
+                    {employee.user_roles.includes("proj_manager") && (<Badge color="gray" size="xl" variant="filled">Project Manager</Badge>)}
                 </div>
 
                 <div className="pt-4 flex flex-col items-center">
-                    <Button className="bg-accent text-white hover:bg-btn_hover font-bold rounded">Make Department manager</Button>
+                    <Button className="bg-accent text-white font-bold rounded-xl text-lg px-5 m-[10px]">Make Department manager</Button>
+                    <Button className="bg-accent text-white font-bold rounded-xl text-lg px-5 m-[10px]">Make Department manager</Button>
+                    <Button className="bg-accent text-white font-bold rounded-xl text-lg px-5 m-[10px]">Make Department manager</Button>
                 </div>
             </Modal>
 
-            <Card className="flex w-[300px] h-[230px] bg-[#505A5E] mx-[40px] my-[20px] rounded-xl dark:text-darktext text-text select-none font-bold" onClick={open}>
+            <Card className="flex w-[300px] h-[230px] bg-[#505A5E] mx-[40px] my-[20px] rounded-xl dark:text-darktext text-text select-none font-bold"
+                onClick={open} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
                 <div className="flex items-center">
                     <Avatar className="m-3 w-[50px] h-[50px] bg-[#E9E5E6]">{getInitials(employee.name)}</Avatar>
                     <div className="flex flex-col">
@@ -44,11 +50,21 @@ export default function EmployeeCard({ employee }) {
                     </div>
                 </div>
                 <div className='ml-3'>{employee.email}</div>
-                <div className="pt-4 flex justify-center">
-                    { !employee.user_roles.length && (<Badge color="gray" size="xl" variant="filled">Employee</Badge>) }
-                    { employee.user_roles.includes("admin") && (<Badge color="gray" size="xl" variant="filled">Organization Admin</Badge>) }
-                    { employee.user_roles.includes("dept_manager") && (<Badge color="gray" size="xl" variant="filled">Department Manager</Badge>) }
-                    { employee.user_roles.includes("proj_manager") && (<Badge color="gray" size="xl" variant="filled">Project Manager</Badge>) }
+                <div className="flex justify-center items-center flex-col text-center h-full">
+                    {!isHovering && !employee.user_roles.length && (
+                        <Badge color="gray" size="xl" variant="filled">Employee</Badge>
+                    )}
+                    {!isHovering && employee.user_roles.includes("admin") && (
+                        <Badge color="gray" size="xl" variant="filled">Organization Admin</Badge>
+                    )}
+                    {!isHovering && employee.user_roles.includes("dept_manager") && (
+                        <Badge color="gray" size="xl" variant="filled">Department Manager</Badge>
+                    )}
+                    {!isHovering && employee.user_roles.includes("proj_manager") && (
+                        <Badge color="gray" size="xl" variant="filled">Project Manager</Badge>
+                    )}
+
+                    {isHovering && <Text className="text-xl">Click to see more!</Text>}
                 </div>
             </Card>
         </>
--- src/components/pages/OrganizationEmployees.jsx
+++ src/components/pages/OrganizationEmployees.jsx
@@ -3,20 +3,24 @@
 
 import React, { useContext, useEffect, useState } from 'react';
 import { Button, Loader, Card } from '@mantine/core';
+
 import { Context } from '../../App';
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import EmployeeCard from '../employeeComponents/EmployeeCard'
 
 export default function OrganizationEmployeesPage() {
 
+    // Initialization
+
     const [darkMode, setDarkMode] = useContext(Context);
     const axiosPrivate = useAxiosPrivate();
     const [users, setUsers] = useState([]);
     const [visible, setVisible] = useState(false);
-
     useEffect(() => {
     }, [darkMode]);
 
+
+    // Function that gets all the users from the organization
 
     useEffect(() => {
         let isMounted = true;
@@ -33,21 +37,62 @@ export default function OrganizationEmployeesPage() {
                 console.error('Error fetching organization members:', error);
             }
             finally {
-                const timeout = 200; 
+                const timeout = 200;
                 setTimeout(() => {
                     setVisible(true);
                 }, timeout);
             }
         }
-        
+
         getUsers();
-        
+
         return () => {
             isMounted = false;
             controller.abort();
         }
     }, [])
-    
+
+    // Function that generates a signup employee link and copies it to clipboard
+
+    function handleSignUpLink() {
+
+        let isMounted = true;
+        const controller = new AbortController();
+
+        const getSignUpLink = async () => {
+            try {
+                const response = await axiosPrivate.post('organizations/signup_token', {
+                    signal: controller.signal
+                });
+
+                const id = response.data.id;
+                const baseUrl = window.location.origin;
+                const urlWithId = `${baseUrl}/signup/${id}`;
+
+                navigator.clipboard.writeText(urlWithId)
+                    .then(() => {
+                        console.log('URL with ID copied to clipboard:', urlWithId);
+                    })
+                    .catch(err => {
+                        console.error('Could not copy URL with ID: ', err);
+                    });
+                // isMounted &&
+            } catch (error) {
+                console.error('Error fetching organization members:', error);
+            }
+        }
+
+        getSignUpLink();
+
+        return () => {
+            isMounted = false;
+            controller.abort();
+        }
+    }
+
+
+    // All the user cards + button to generate signup employee link
+
     return (
         <div className={`${darkMode && 'dark'}`}>
             <div className='dark:bg-darkcanvas bg-canvas h-screen flex flex-wrap'>
@@ -57,20 +102,24 @@ export default function OrganizationEmployeesPage() {
                     </div>
                 )}
                 {visible && (
-                    Object.keys(users).length > 0 ?
-                        Object.keys(users).map(user_id => (
-                            <EmployeeCard key={user_id} employee={users[user_id]} />
-                        )) : (
-                            <div className="flex">
+                    <>
+                        <div className='flex flex-wrap gap-6'>
+                            {Object.keys(users).length > 0 ? (
+                                Object.keys(users).map(user_id => (
+                                    <EmployeeCard key={user_id} employee={users[user_id]} />
+                                ))
+                            ) : (
                                 <Card className='w-[300px] h-[230px] bg-accent mx-[40px] my-[20px] rounded-xl dark:text-darktext text-text select-none flex justify-center items-center'>
                                     <h2 className='text-2xl block'>You have no employees</h2>
                                 </Card>
-                                <Button className='w-[300px] h-[230px] mx-[40px] my-[20px] rounded-xl select-none
-                                               bg-accent text-white text-2xl hover:bg-btn_hover font-bold text-white'>
-                                    Need more employees
-                                </Button>
-                            </div>
-                        ))}
+                            )}
+                            <Button className='w-[300px] h-[230px] mx-[40px] my-[20px] rounded-xl select-none bg-accent text-white text-2xl hover:bg-btn_hover font-bold text-white'
+                                onClick={handleSignUpLink}>
+                                Need more employees
+                            </Button>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     )
