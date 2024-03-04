f80aba30dc499b00fa573c16a3d4e1109b4fd0c1
duda4418 <146849849+duda4418@users.noreply.github.com>
2024-03-04 18:55:14 +0200

    Implementing org departments

--- src/components/pages/OrganizationDepartments.jsx
+++ src/components/pages/OrganizationDepartments.jsx
@@ -1,10 +1,9 @@
 /* eslint-disable no-unused-vars */
 
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
-
+import { Button } from '@mantine/core';
 import { useHeadroom } from '@mantine/hooks';
-
 import { Context } from '../../App';
 import DepartmentCard from '../skillComponents/DepartmentCard';
 
@@ -13,22 +12,32 @@ export default function OrganizationDepartmentsPage() {
     const [darkMode, setDarkMode] = useContext(Context);
     const pinned = useHeadroom({ fixedAt: 20 });
     const axiosPrivate = useAxiosPrivate();
+    const [departments, setDepartments] = useState([]);
 
+    // GET DEPARTMENTS 
     useEffect(() => {
+        let isMounted = true;
         const controller = new AbortController();
-        const fetchUserSkills = async () => {
-            try {
+        const fetchDepartments = async () => {
+          try {
                 const response = await axiosPrivate.get('departments', {
                     signal: controller.signal
                 });
                 console.log('Departments:', response.data);
+                isMounted && setDepartments(response.data);
+
             } catch (error) {
                 console.error('Error fetching departments:', error);
             }
+            finally {
+                isMounted = false;
+                controller.abort();
+            }
         }
-        fetchUserSkills();
+        fetchDepartments();
         return () => {
-            
+            isMounted = false;
+            controller.abort();
         }
     }, []);
 
@@ -40,7 +49,21 @@ export default function OrganizationDepartmentsPage() {
         <>
             <div className={`${darkMode && 'dark'}`}>
                 <div className='dark:bg-darkcanvas bg-canvas h-auto select-none'>
-                    <DepartmentCard />
+                    <div className="flex flex-wrap">
+                        {departments.map((department, index) => (
+                            <DepartmentCard  key={index} leader={department.manager_name} name={department.name} members={department.department_members}/>
+                        ))}    
+                        <div className="w-[200px] h-[270px] flex justify-center items-center">
+                            <Button variant="outline"
+                                className={`relative w-[80px] h-[80px] m-[6px] rounded-full p-0 text-accent border-accent border-[5px] hover:text-accent`}>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus w-full h-full" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M12 5l0 14" />
+                                    <path d="M5 12l14 0" />
+                                </svg>
+                            </Button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
