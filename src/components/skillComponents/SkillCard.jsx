5818b808272d97040288fbecf8b0a75bc01e57a5
David Rotariu <davidrotariu139@gmail.com>
2024-03-04 12:55:07 +0200

    edit org skills

--- src/components/skillComponents/SkillCard.jsx
+++ src/components/skillComponents/SkillCard.jsx
@@ -1,24 +1,54 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { Card, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { Modal, Button } from '@mantine/core';
+import { Modal, Button, Title, TextInput, Textarea } from '@mantine/core';
+import { useState, useEffect } from 'react'
 
 export const SkillCard = ({ skill }) => {
 
     const [opened, { open, close }] = useDisclosure(false);
+    const [isHovering, setIsHovering] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const [name, setName] = useState(`${skill.name}`);
+    const [description, setDescription] = useState(`${skill.description}`);
+
+    const handleEdit = () => {
+        setIsEditing(true);
+    };
+
+    const handleSave = () => {
+        skill.name = name;
+        skill.description = description;
+        setIsEditing(false);
+    };
 
     return (
         <div>
-            <Modal opened={opened} onClose={close} centered overflow="inside" className="bg-.m-1b7284a3.m-b5489c3c text-white rounded-modal" zIndex={1000002}>
-                <div className="flex justify-center">
-                    <h1 className="text-3xl font-bold">{skill.name}</h1>
+            <Modal opened={opened} onClose={close} centered overflow="inside" className="bg-.m-1b7284a3.m-b5489c3c text-white rounded-modal select-none" zIndex={1000002}>
+                <div className="flex justify-center justify-center h-[50px]">
+                    {!isEditing && <h1 className="text-3xl font-bold">{skill.name}</h1>}
+                    {isEditing && <TextInput
+                        placeholder="Skill name"
+                        value={name}
+                        onChange={(event) => setName(event.currentTarget.value)}
+                        size="lg"
+                    />}
                 </div>
                 <div className="pt-4 flex justify-left">
                     <p className="font-bold">Author</p>
                     <p>: {skill.author_name}</p>
                 </div>
                 <div className="pt-4 flex justify-left">
-                    <p><span className="font-bold">Description</span>: {skill.description}</p>
+                    {!isEditing && (<p className="text-[15px]"><span className="font-bold">Description</span>: {skill.description}</p>)}
+                    {isEditing && <Textarea
+                        placeholder="Description"
+                        value={description}
+                        onChange={(event) => setDescription(event.currentTarget.value)}
+                        autosize
+                        className="w-full text-[12px]"
+                    />}
                 </div>
                 <div className="pt-4 flex justify-left">
                     <p><span className="font-bold">Category</span>: {skill.category_name}</p>
@@ -27,18 +57,29 @@ export const SkillCard = ({ skill }) => {
                     <p><span className="font-bold">Departments</span>: {skill.dept_name.join(', ')}</p>
                 </div>
                 <div className="pt-4 flex justify-left">
-                    <Button className="bg-accent text-white hover:bg-btn_hover font-bold rounded">Edit skill</Button>
+                    {!isEditing && (<Button className="bg-accent text-white hover:bg-btn_hover font-bold rounded" onClick={handleEdit}>
+                        Edit skill
+                    </Button>)}
+                    {isEditing && (<Button className="bg-accent text-white hover:bg-btn_hover font-bold rounded" onClick={handleSave}>
+                        Save
+                    </Button>)}
                 </div>
             </Modal>
 
-            <Card variant="filled" onClick={open}
-                className="bg-graybg hover:bg-btn_hover w-[250px] h-[154px] mx-[40px] font-bold text-white">
-                <Card.Section className="bg-[#495256] p-3 text-2xl  flex justify-center items-center">
-                    {skill.name}
+            <Card variant="filled" onClick={open} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}
+                className="flex w-[240px] h-[184px] dark:bg-card_modal mx-[40px] my-[20px] rounded-xl dark:text-darktext text-text select-none font-bold">
+                <Card.Section className="dark:bg-[#495256]">
+                    <Title className="p-4 flex justify-center text-3xl">
+                        {skill.name}
+                    </Title>
                 </Card.Section>
-                <Text>
-
-                </Text>
+                <div className="p-4">
+                    <Text className="text-[16px]">
+                        {!isHovering && skill.dept_name && `Departments: ${skill.dept_name.join(', ')}`}
+                        {!isHovering && !skill.dept_name.length && "Not used in any departments"}
+                        {isHovering && <Text className="text-xl">Click to see more!</Text>}
+                    </Text>
+                </div>
             </Card>
         </div>
     )
