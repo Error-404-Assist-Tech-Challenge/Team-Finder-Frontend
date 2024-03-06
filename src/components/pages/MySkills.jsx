5116f8efdda0f7e3a14742b449e63d30f0d79bd0
duda4418 <146849849+duda4418@users.noreply.github.com>
2024-03-06 23:06:41 +0200

    Refactoring for pagination

--- src/components/pages/MySkills.jsx
+++ src/components/pages/MySkills.jsx
@@ -5,10 +5,9 @@ import { useState, useEffect, useContext } from 'react';
 import { Loader, rem, Card, Modal, Button, Select, Title } from '@mantine/core';
 import { useHeadroom, useDisclosure } from '@mantine/hooks';
 import { Context } from '../../App';
-import UserSkillCard from '../skillComponents/UserSkillCard';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
-import ExperienceCirclesSelected from '../skillComponents/ExperienceCirclesSelected';
-import LevelCirclesSelected from '../skillComponents/LevelCirclesSelected';
+import MySkillsComp from '../pageComponents/MySkillsComp';
+import PaginationComp from '../pageComponents/Pagination';
 
 export default function MySkillsPage() {
 
@@ -26,6 +25,12 @@ export default function MySkillsPage() {
 
     const [unusedSkills, setUnusedSkills] = useState([]);
     const [addedSkill, setAddedSkill] = useState('');
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postPerPage, setPostPerPage] = useState(9);
+    const lastPostIndex = currentPage * postPerPage;
+    const firstPostIndex = lastPostIndex - postPerPage;
+    const currentPosts =  skills.slice(firstPostIndex, lastPostIndex);
 
     const language = unusedSkills.find(lang => lang.value === addedSkill);
 
@@ -91,40 +96,6 @@ export default function MySkillsPage() {
         }
     }, []);
 
-    // Add new skill to user
-
-    const handleAddSkill = async () => {
-        try {
-            const response = await axiosPrivate.post('skills/user',
-                JSON.stringify({
-                    skill_id: addedSkill,
-                    level: selectedSkillLevel,
-                    experience: selectedSkillExperience
-                }),
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin': '*',
-                        'Access-Control-Allow-Credentials': 'true'
-                    },
-                    withCredentials: true
-                });
-
-            console.log('Response:', response.data);
-
-            setSkills(response.data);
-
-            const newUnusedSkills = unusedSkills.filter(skill => skill.value !== addedSkill);
-            setUnusedSkills(newUnusedSkills);
-
-            setAddedSkill('')
-
-        } catch (error) {
-            console.error('Error fetching unused skills:', error);
-        }
-        close();
-    }
-
     useEffect(() => {
         setChange(true);
     }, [skills]);
@@ -134,95 +105,20 @@ export default function MySkillsPage() {
 
     return (
         <div className={`${darkMode && 'dark'}`}>
-            <div className='dark:bg-darkcanvas bg-canvas h-auto select-none'>
+            <div className='dark:bg-darkcanvas bg-canvas h-auto min-h-screen select-none'>
                 {visible && (
                     <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                         <Loader size={30} color="red" />
                     </div>
                 )}
-                <Modal opened={opened} onClose={close} centered overflow="inside" size={500} className="dark:bg-card_modal text-white select-none" zIndex={1000002}>
-                    <div className="flex flex-col space-y-4 ">
-                        <div className="flex flex-col justify-center items-center">
-                            <Title className="pb-[30px]">Add Skill</Title>
-                        </div>
-                        <div>
-                            <Select
-                                label="Skill"
-                                placeholder="Choose a skill"
-                                data={unusedSkills}
-                                value={addedSkill}
-                                onChange={setAddedSkill}
-                                searchable
-                                size="md"
-                                nothingFoundMessage="Skill does not exist..."
-                                comboboxProps={{ zIndex: 1000000000 }}
-                                className="pb-[30px]" />
-                        </div>
-                        <div className="p-3 flex justify-left text-xl">
-                            <p>
-                                <span className="font-bold">Level: </span>
-                                {addedSkill && (
-                                    <span>
-                                        {selectedSkillLevel == 1 && `You are learning ${language.label}`}
-                                        {selectedSkillLevel == 2 && `You know ${language.label}`}
-                                        {selectedSkillLevel == 3 && `You do ${language.label}`}
-                                        {selectedSkillLevel == 4 && `You can help in ${language.label}`}
-                                        {selectedSkillLevel == 5 && `You can teach ${language.label}`}
-                                    </span>
-                                )}
-                                {!addedSkill && (
-                                    <span> Please select a skill! </span>
-                                )}
-                            </p>
-                        </div>
-                        <div className="flex justify-center items-center flex-col text-center">
-                            <LevelCirclesSelected selectedSkillLevel={selectedSkillLevel} selectSkillLevel={selectSkillLevel} />
-                        </div>
-                        <div className="p-3 flex justify-left text-xl">
-                            <p>
-                                <span className="font-bold">Experience: </span>
-                                {addedSkill && (
-                                    <span>
-                                        {selectedSkillExperience == 1 && "0-6 months"}
-                                        {selectedSkillExperience == 2 && "6-12 months"}
-                                        {selectedSkillExperience == 3 && "1-2 years"}
-                                        {selectedSkillExperience == 4 && "2-4 years"}
-                                        {selectedSkillExperience == 5 && "4-7 years"}
-                                        {selectedSkillExperience == 6 && "7+ years"}
-                                    </span>
-                                )}
-                                {!addedSkill && (
-                                    <span>Please select a skill! </span>
-                                )}
-
-                            </p>
-                        </div>
-                        <div className="flex justify-center items-center flex-col text-center pb-[20px]">
-                            <ExperienceCirclesSelected selectedSkillExperience={selectedSkillExperience} selectSkillExperience={selectSkillExperience} />
-                            {addedSkill && (<Button className="bg-accent text-white hover:bg-btn_hover font-bold  py-2 rounded mx-[10px] mt-[30px] mb-[10px] float-right"
-                                onClick={handleAddSkill} style={{ width: '460px' }}>
-                                Add Skill
-                            </Button>)}
-                        </div>
-                    </div>
-                </Modal>
+                
                 {!visible &&
                     <div className="flex flex-wrap">
-                        {skills.map((skill, index) => (
-                            <UserSkillCard key={index}
-                                index={index} skills={skills} setSkills={setSkills} unusedSkills={unusedSkills} setUnusedSkills={setUnusedSkills} />
-                        ))}
-                        <div className="w-[200px] h-[270px] flex justify-center items-center">
-                            <Button variant="outline" onClick={open}
-                                className={`relative w-[80px] h-[80px] m-[6px] rounded-full p-0 text-accent border-accent border-[5px] hover:text-accent`}>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus w-full h-full" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path d="M12 5l0 14" />
-                                    <path d="M5 12l14 0" />
-                                </svg>
-                            </Button>
-                        </div>
+                            <MySkillsComp  skills={skills} setSkills={setSkills} unusedSkills={unusedSkills} setUnusedSkills={setUnusedSkills} />
                     </div>}
+            </div>
+            <div className='dark:bg-darkcanvas bg-canvas flex justify-center items-center'>
+                <PaginationComp totalPosts={skills.length} postsPerPage={postPerPage} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
             </div>
         </div>
     )
--- src/components/pages/OrganizationDepartments.jsx
+++ src/components/pages/OrganizationDepartments.jsx
@@ -5,7 +5,8 @@ import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import { Button, Modal, TextInput, Loader, Title } from '@mantine/core';
 import { useHeadroom, useDisclosure } from '@mantine/hooks';
 import { Context } from '../../App';
-import DepartmentCard from '../departmentComponents/DepartmentCard';
+import OrganizationDepartmentsComp from '../pageComponents/OrganizationDepartmentsComp';
+import PaginationComp from '../pageComponents/Pagination';
 
 export default function OrganizationDepartmentsPage() {
 
@@ -13,10 +14,15 @@ export default function OrganizationDepartmentsPage() {
     const pinned = useHeadroom({ fixedAt: 20 });
     const axiosPrivate = useAxiosPrivate();
     const [departments, setDepartments] = useState([]);
-    const [opened, { open, close }] = useDisclosure(false);
     const [departmentName, setDepartmentName] = useState('');
     const [departmentManagers, setDepartmentManagers] = useState([]);
     const [visible, setVisible] = useState(true);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postPerPage, setPostPerPage] = useState(9);
+    const lastPostIndex = currentPage * postPerPage;
+    const firstPostIndex = lastPostIndex - postPerPage;
+    const currentPosts =  departments.slice(firstPostIndex, lastPostIndex);
 
     // GET DEPARTMENTS 
 
@@ -81,33 +87,7 @@ export default function OrganizationDepartmentsPage() {
     }, []);
 
 
-    // ADD DEPARTMENT
-
-    const handleAddDepartment = async () => {
-        try {
-            const response = await axiosPrivate.post('departments',
-                JSON.stringify({
-                    name: departmentName,
-                }),
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin': '*',
-                        'Access-Control-Allow-Credentials': 'true'
-                    },
-                    withCredentials: true
-                });
-
-            console.log('Response:', response.data);
-
-            setDepartments(response.data);
-
-        } catch (error) {
-            console.error('Error fetching unused skills:', error);
-        }
-
-        close();
-    }
+    
 
     useEffect(() => {
 
@@ -117,53 +97,23 @@ export default function OrganizationDepartmentsPage() {
     return (
         <>
             <div className={`${darkMode && 'dark'}`}>
-                <div className='dark:bg-darkcanvas bg-canvas h-auto select-none'>
+                <div className='dark:bg-darkcanvas bg-canvas h-auto min-h-screen select-none'>
                     {visible && (
                         <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                             <Loader size={30} color="red" />
                         </div>
                     )}
-                    <Modal opened={opened} onClose={close} centered overflow="inside" size={500} className="dark:bg-card_modal text-white select-none" zIndex={1000002}>
-                        <div className="flex justify-center">
-                            <Title className="pb-[30px]">Create Department</Title>
-                        </div>
-                        <TextInput
-                            label="Department Name"
-                            placeholder="Department name"
-                            size="lg"
-                            value={departmentName}
-                            onChange={(event) => setDepartmentName(event.currentTarget.value)}
-                            className=" py-[30px]"
-                        />
-                        {departmentName &&
-                            (<Button onClick={handleAddDepartment}
-                                className="bg-accent text-white hover:bg-btn_hover font-bold px-4 py-2 rounded mx-[10px] my-[10px] mt-[20px]">
-                                Create Department
-                            </Button>)}
-                    </Modal>
-
                     {!visible && (
                         <div className="flex flex-wrap justify-centerbg-darkcanvas">
                             <div className='bg-darkcanvas select-none h-auto py-[30px] flex flex-wrap'>
-                                {departments.map((department, index) => (
-                                    <DepartmentCard key={index}
-                                        id={department.id} manager={department.manager_name} manager_id={department.manager_id} name={department.name} members={department.department_members}
-                                        departmentManagers={departmentManagers} setDepartmentManagers={setDepartmentManagers}
-                                        departments={departments} setDepartments={setDepartments} />
-                                ))}
-                                <div className="w-[200px] h-[224px] flex justify-center items-center">
-                                    <Button variant="outline" onClick={open}
-                                        className={`relative w-[80px] h-[80px] m-[6px] rounded-full p-0 text-accent border-accent border-[5px] hover:text-accent`}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus w-full h-full" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path d="M12 5l0 14" />
-                                            <path d="M5 12l14 0" />
-                                        </svg>
-                                    </Button>
-                                </div>
+                                    <OrganizationDepartmentsComp departmentManagers={departmentManagers} setDepartmentManagers={setDepartmentManagers}
+                                                                 departments={departments} setDepartments={setDepartments} />
                             </div>
                         </div>
                     )}
+                </div>
+                <div className='dark:bg-darkcanvas bg-canvas flex justify-center items-center'>
+                    <PaginationComp totalPosts={departments.length} postsPerPage={postPerPage} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
                 </div>
             </div>
         </>
