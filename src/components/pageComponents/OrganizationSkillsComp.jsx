5a59902fe3bfc395338ae94642b7e42d9464d1cc
David Rotariu <davidrotariu139@gmail.com>
2024-03-08 17:52:34 +0200

    org skill filters

--- src/components/pageComponents/OrganizationSkillsComp.jsx
+++ src/components/pageComponents/OrganizationSkillsComp.jsx
@@ -4,7 +4,7 @@
 /* eslint-disable no-unused-vars */
 
 import { useState } from 'react';
-import { Loader, Button, Checkbox, Modal, TextInput, Title, Textarea, Select } from '@mantine/core';
+import { Loader, Button, Checkbox, Modal, TextInput, Title, Textarea, Text, Select } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import { SkillCard } from '../skillComponents/SkillCard';
@@ -19,6 +19,10 @@ export default function OrganizationSkillsComp({ skills, skillCategories, setSki
     const [skillDescription, setSkillDescription] = useState('')
     const [skillCategory, setSkillCategory] = useState('')
     const [visible, setVisible] = useState(false);
+
+    const [categoryFilter, setCategoryFilter] = useState('')
+    const [authorFilter, setAuthorFilter] = useState(false)
+    const [departmentFilter, setDepartmentFilter] = useState(false)
 
     const handleAddSkill = async () => {
         closeSkills();
@@ -52,6 +56,19 @@ export default function OrganizationSkillsComp({ skills, skillCategories, setSki
         }
         setVisible(false)
     }
+
+    const filteredSkills = skills.filter(skill => {
+        if (departmentFilter && !skill.is_department_managed) {
+            return false;
+        }
+        if (categoryFilter && skill.category_id !== categoryFilter) {
+            return false;
+        }
+        if (authorFilter && !skill.is_authored) {
+            return false;
+        }
+        return true;
+    });
 
 
     return (
@@ -94,36 +111,64 @@ export default function OrganizationSkillsComp({ skills, skillCategories, setSki
                         Create Skill
                     </Button>)}
             </Modal>
-            <div className="w-full flex justify-center text-white pb-5" size="2xl">
-                <Title>Skills</Title>
+            <div className="w-full flex justify-center text-white pb-5" >
+                <Title size="45">Skills</Title>
             </div>
-            <Checkbox className="text-white" label="Only show my skills" />
+
+            <div className="flex items-center justify-between my-5 w-full">
+                <div className="w-1/3 flex justify-left">
+                    <Checkbox className="text-white mx-10" label="Skills in my department" size="xl"
+                        checked={departmentFilter}
+                        onChange={(event) => setDepartmentFilter(event.currentTarget.checked)} />
+                </div>
+                <div className="w-1/3 flex justify-center">
+                    <Select
+                        placeholder="Select a category"
+                        data={skillCategories}
+                        value={categoryFilter}
+                        onChange={setCategoryFilter}
+                        searchable
+                        size="md"
+                        nothingFoundMessage="Category does not exist..."
+                        comboboxProps={{ zIndex: 1000000000 }}
+                        className="mx-5" />
+                </div>
+                <div className="w-1/3 flex justify-left">
+                    <Checkbox className="text-white mx-10" label="Skills created by me" size="xl"
+                        checked={authorFilter}
+                        onChange={(event) => setAuthorFilter(event.currentTarget.checked)} />
+                </div>
+            </div >
+
             {visible && (
                 <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/5">
                     <Loader size={30} color="red" />
                 </div>
-            )}
-            {!visible && (
-                <>
-                    <div className='flex flex-wrap justify-center'>
-                        {skills.map((skill, index) => (
-                            <SkillCard key={index} skill={skill} skillCategories={skillCategories} setSkills={setSkills} visible={visible} setVisible={setVisible} />
-                        ))}
-                        <div className='dark:bg-darkcanvas bg-darkcanvas'>
-                            <div className="w-[320px] h-[224px] flex justify-center items-center">
-                                <Button variant="outline" onClick={openSkills}
-                                    className={`relative w-[80px] h-[80px] m-[6px] rounded-full p-0 text-accent border-accent border-[5px] hover:text-accent`}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus w-full h-full" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                        <path d="M12 5l0 14" />
-                                        <path d="M5 12l14 0" />
-                                    </svg>
-                                </Button>
+            )
+            }
+            {
+                !visible && (
+                    <>
+                        <div className='flex flex-wrap justify-center'>
+                            {filteredSkills.map((skill, index) => (
+                                <SkillCard key={index} skill={skill} skillCategories={skillCategories} setSkills={setSkills} visible={visible} setVisible={setVisible} />
+                            ))}
+                            <div className='dark:bg-darkcanvas bg-darkcanvas'>
+                                <div className="w-[320px] h-[224px] flex justify-center items-center">
+                                    <Button variant="outline" onClick={openSkills}
+                                        className={`relative w-[80px] h-[80px] m-[6px] rounded-full p-0 text-accent border-accent border-[5px] hover:text-accent`}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus w-full h-full" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M12 5l0 14" />
+                                            <path d="M5 12l14 0" />
+                                        </svg>
+                                    </Button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </>
-            )}
+                    </>
+                )
+            }
         </>
     )
 }
