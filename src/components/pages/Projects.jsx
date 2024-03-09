91d2a5b93378f965288326f8fe4abec5ee0147c6
David Rotariu <davidrotariu139@gmail.com>
2024-03-09 18:52:59 +0200

    started basics projects page

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,51 +1,182 @@
 /* eslint-disable no-unused-vars */
 
-import React, { useContext, useEffect } from 'react';
-import { useDisclosure } from '@mantine/hooks';
-import { useState } from 'react';
-import { useHeadroom } from '@mantine/hooks';
-import { Card, Text, Title, Modal } from '@mantine/core';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHeadroom, useDisclosure } from '@mantine/hooks';
 import { Context } from '../../App';
+import ProjectCard from '../projectComponents/ProjectCard';
+import { Button, Modal, Title, TextInput, Textarea, Select } from '@mantine/core';
+import { DateInput } from '@mantine/dates';
+import '@mantine/dates/styles.css';
 
 export default function ProjectsPage() {
 
     const [darkMode, setDarkMode] = useContext(Context);
     const pinned = useHeadroom({ fixedAt: 20 });
-    const [isHovering, setIsHovering] = useState(false);
     const [opened, { open, close }] = useDisclosure(false);
 
     useEffect(() => {
 
     }, [darkMode]);
 
+    const projects = [
+        {
+            "name": "E-commerce Platform Dev",
+            "period": "Fixed",
+            "start_date": "2024-02-01",
+            "deadline_date": "2024-08-01",
+            "status": "Starting",
+            "description": "Building an e-commerce platform for online retail.",
+            "tech_stack": ["PHP", "MySQL", "Laravel", "Vue.js"],
+            "roles": [
+                { "role": "Backend Developer", "members": 3 },
+                { "role": "Frontend Developer", "members": 2 },
+                { "role": "UI/UX Designer", "members": 1 },
+                { "role": "Project Manager", "members": 1 }
+            ]
+        },
+        {
+            "name": "Mobile App Dev",
+            "period": "Ongoing",
+            "start_date": "2023-11-15",
+            "status": "In Progress",
+            "description": "Creating a mobile app for both iOS and Android platforms.",
+            "tech_stack": ["Swift", "Kotlin", "Firebase"],
+            "roles": [
+                { "role": "iOS Developer", "members": 2 },
+                { "role": "Android Developer", "members": 2 },
+                { "role": "Backend Developer", "members": 1 },
+                { "role": "UI/UX Designer", "members": 1 },
+                { "role": "QA Engineer", "members": 1 }
+            ]
+        },
+        {
+            "name": "Data Analysis Dashboard",
+            "period": "Fixed",
+            "start_date": "2023-09-01",
+            "deadline_date": "2023-12-15",
+            "status": "Closing",
+            "description": "Developing a dashboard for analyzing and visualizing data.",
+            "tech_stack": ["Python", "Pandas", "Matplotlib", "React"],
+            "roles": [
+                { "role": "Data Scientist", "members": 2 },
+                { "role": "Frontend Developer", "members": 1 },
+                { "role": "Backend Developer", "members": 1 },
+                { "role": "Project Manager", "members": 1 }
+            ]
+        },
+        {
+            "name": "Cloud Migration",
+            "period": "Fixed",
+            "start_date": "2024-01-10",
+            "deadline_date": "2024-05-01",
+            "status": "In Progress",
+            "description": "Migrating on-premises infrastructure to cloud services.",
+            "tech_stack": ["AWS", "Azure", "Docker", "Kubernetes"],
+            "roles": [
+                { "role": "Cloud Architect", "members": 2 },
+                { "role": "DevOps Engineer", "members": 2 },
+                { "role": "Project Manager", "members": 1 }
+            ]
+        },
+        {
+            "name": "Marketing Automation",
+            "period": "Ongoing",
+            "start_date": "2024-02-15",
+            "status": "Not Started",
+            "description": "Automating marketing campaign processes to improve efficiency.",
+            "tech_stack": ["Python", "Django", "PostgreSQL"],
+            "roles": [
+                { "role": "Backend Developer", "members": 2 },
+                { "role": "UI/UX Designer", "members": 1 },
+                { "role": "Project Manager", "members": 1 }
+            ]
+        }
+    ];
+
+    const [projectName, setProjectName] = useState('')
+    const [projectPeriod, setProjectPeriod] = useState('')
+    const [projectStartDate, setProjectStartDate] = useState(null);
+    const [projectDescription, setProjectDescription] = useState('')
+
+
     return (
         <div className={`${darkMode && 'dark'}`}>
-            <Modal  opened={opened}
-                    onClose={close}
-                    title="This is a fullscreen modal"
-                    fullScreen
-                    radius={0}
-                    transitionProps={{ transition: 'fade', duration: 200 }} className="dark:bg-card_modal text-white select-none" zIndex={1000002}>
-                Project details
-            </Modal>
             <div className='dark:bg-darkcanvas bg-canvas h-auto min-h-screen select-none'>
-                <Card className="flex w-[330px] h-[230px] dark:bg-card_modal mx-[40px] my-[00px] rounded-xl dark:text-darktext text-text select-none font-bold"
-                       onClick={open} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
-                    <Card.Section className="dark:bg-[#495256]">
-                        <Title className="p-4 flex justify-center">
-                            25H usv
-                        </Title>
-                    </Card.Section>
-                    <div className="flex justify-center items-center flex-col text-center h-full">
-                        {!isHovering && (
-                            <>
-                                <Text>Members:</Text>
-                                <Text>Due to:</Text>
-                            </>
-                        )}
-                        {isHovering && <Text className="text-xl">Click to see more!</Text>}
+                <Modal opened={opened} onClose={close} centered overflow="inside" size={500} className="dark:bg-card_modal text-white select-none" zIndex={1000002}>
+                    <div className="flex justify-center">
+                        <Title className="pb-[20px]">Create Project</Title>
                     </div>
-                </Card>
+                    <TextInput
+                        label="Project Name"
+                        placeholder="Project name..."
+                        size="md"
+                        value={projectName}
+                        onChange={(event) => setProjectName(event.currentTarget.value)}
+                        className=" py-[5px]"
+                    />
+                    <Select
+                        label="Project Period"
+                        allowDeselect={false}
+                        placeholder="Select a period..."
+                        data={['Fixed', 'Ongoing']}
+                        value={projectPeriod}
+                        onChange={setProjectPeriod}
+                        size="sm"
+                        comboboxProps={{ zIndex: 1000000000 }}
+                        className="py-[5px]" />
+
+                    <DateInput
+                        value={projectStartDate}
+                        onChange={setProjectStartDate}
+                        label="Start Date"
+                        placeholder="Start date..."
+                    />
+
+                    <Textarea
+                        label="Project Description"
+                        placeholder="Project description..."
+                        value={projectDescription}
+                        onChange={(event) => setProjectDescription(event.currentTarget.value)}
+                        className=" py-[15px]"
+                    />
+
+                    <Select
+                        label="Skill Category"
+                        allowDeselect={false}
+                        placeholder="Select a category"
+                        // data={skillCategories}
+                        // value={skillCategory}
+                        // onChange={setSkillCategory}
+                        searchable
+                        size="md"
+                        nothingFoundMessage="Category does not exist..."
+                        comboboxProps={{ zIndex: 1000000000 }}
+                        className="py-[15px]" />
+                    {/* {skillName && skillDescription && skillCategory && ( */}
+                    <div className="flex justify-center">
+                        <Button
+                            size="lg"
+                            className="bg-accent text-white hover:bg-btn_hover font-bold px-4 py-2 rounded mb-[10px] mt-[20px]">
+                            Create Skill
+                        </Button>
+                    </div>
+                    {/* )} */}
+                </Modal >
+                <div className="flex flex-wrap justify-center">
+                    {projects.map((project, index) => (
+                        <ProjectCard key={index} project={project} />
+                    ))}
+                    <div className="w-[350px] h-[280px] mx-[40px] my-[40px] flex justify-center items-center">
+                        <Button variant="outline" onClick={open}
+                            className={`relative w-[80px] h-[80px] m-[6px] rounded-full p-0 text-accent border-accent border-[5px] hover:text-accent`}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus w-full h-full" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M12 5l0 14" />
+                                <path d="M5 12l14 0" />
+                            </svg>
+                        </Button>
+                    </div>
+                </div>
             </div>
         </div>
     )
---
+++ src/components/projectComponents/ProjectCard.jsx
@@ -0,0 +1,95 @@
+/* eslint-disable react/prop-types */
+import { Card, Badge, Title, Modal, Divider, Button } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { useState } from 'react';
+
+export default function ProjectCard({ project }) {
+
+    const [isHovering, setIsHovering] = useState(false);
+    const [opened, { open, close }] = useDisclosure(false);
+
+    return (
+        <>
+            <Modal opened={opened} onClose={close} size="calc(100vw - 3rem)" transitionProps={{ transition: 'fade', duration: 200 }} className="dark:bg-card_modal text-white select-none" zIndex={1000002}>
+                <div className="h-[80vh] flex">
+                    <div className="w-1/2">
+                        <Title className="flex justify-center">
+                            {project.name}
+                        </Title>
+                        <div className="text-[20px] p-9">
+                            <p className="py-1"><span className="font-bold">Period</span>: {project.period}</p>
+                            <p className="py-1"><span className="font-bold">Start Date</span>: {project.start_date}</p>
+                            {project.deadline_date && (
+                                <p className="py-1"><span className="font-bold">Deadline Date</span>: {project.deadline_date}</p>
+                            )}
+                            <p className="py-1"><span className="font-bold">Status</span>: {project.status}</p>
+                            <p className="py-1"><span className="font-bold">Description</span>: {project.description}</p>
+                            <div className="flex items-center flex-wrap">
+                                <p className="py-1"><span className="font-bold">Technology Stack</span>: </p>
+                                {project.tech_stack.map((tech, index) => (
+                                    <Badge key={index} className="mx-3 my-1" color="gray" size="xl">{tech}</Badge>
+                                ))}
+                            </div>
+                            <div className="flex items-center flex-wrap">
+                                <p className="py-1"><span className="font-bold">Team Roles</span>: </p>
+                                {project.roles.map((role, index) => (
+                                    <Badge key={index} className="mx-3 my-1" color="gray" size="xl">
+
+                                        {/* <Button variant="outline"
+                                            className={`w-[20px] h-[20px] m-[6px] rounded-full p-0 text-accent border-accent border-[3px]`}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-minus w-[12px] h-[12px]" width="24" height="24" viewBox="0 0 24 24" strokeWidth="4" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M5 12l14 0" />
+                                            </svg>
+                                        </Button> */}
+
+                                        {role.members}x {role.role}
+
+                                        {/* <Button variant="outline"
+                                            className={`w-[20px] h-[20px] m-[6px] rounded-full p-0 text-accent border-accent border-[3px]`}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus w-[15px] h-[15px]" viewBox="0 0 24 24" strokeWidth="4" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M12 5l0 14" />
+                                                <path d="M5 12l14 0" />
+                                            </svg>
+                                        </Button> */}
+
+                                    </Badge>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <Divider className="color-white" size="md" orientation="vertical" />
+                    <div className="w-1/2">
+                        <Title className="flex justify-center">
+                            Team Finder
+                        </Title>
+                    </div>
+                </div>
+            </Modal>
+            <Card className="flex w-[350px] h-[280px] dark:bg-card_modal mx-[40px] my-[40px] rounded-xl dark:text-darktext text-text select-none"
+                onClick={open} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
+                <Card.Section className="dark:bg-[#495256]">
+                    <Title className="py-6 px-2 flex justify-center text-center text-[28px]">
+                        {project.name}
+                    </Title>
+                </Card.Section>
+                <div className="flex h-full justify-center ml-4 flex-col">
+                    {!isHovering && (
+                        <div className="text-[18px]">
+                            <p className="py-1"><span className="font-bold">Period</span>: {project.period}</p>
+                            <p className="py-1"><span className="font-bold">Start Date</span>: {project.start_date}</p>
+                            {project.deadline_date && (
+                                <p className="py-1"><span className="font-bold">Deadline Date</span>: {project.deadline_date}</p>
+                            )}
+                            <p className="py-1"><span className="font-bold">Status</span>: {project.status}</p>
+                        </div>
+                    )}
+                    {isHovering && (
+                        <p className="text-xl text-center">Click to see more!</p>
+                    )}
+                </div>
+            </Card>
+        </>
+    )
+}
