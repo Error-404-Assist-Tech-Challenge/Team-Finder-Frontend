5eec635d4de2e320d825bb9a1849d75f9353eb46
duda4418 <146849849+duda4418@users.noreply.github.com>
2024-03-12 19:30:41 +0200

    Paginated Proposed Members

---
+++ src/components/projectComponents/ProposedMembersComp.jsx
@@ -0,0 +1,11 @@
+import ProposedMemberCard from "./ProposedMemberCard"
+
+export default function ProposedMembersComp({proposedMembers, available_roles, project_id}){
+    return(
+        <>
+            {proposedMembers.map((employee, index) => (
+                <ProposedMemberCard key={index} employee={employee} available_roles={available_roles} project_id={project_id} />
+            ))}
+        </>
+    )
+}
